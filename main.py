f21de021aa5f0634930c23d019648da1d271f17d
Gennadynemchin <69091759+Gennadynemchin@users.noreply.github.com>
2022-02-17 19:04:08 +0700

    changed name for functions argument bitly_token

--- main.py
+++ main.py
@@ -6,24 +6,24 @@ from dotenv import load_dotenv
 from requests.models import HTTPError
 
 
-def is_bitlink(link, auth) -> bool:
+def is_bitlink(link, bitly_token) -> bool:
     url = f'https://api-ssl.bitly.com/v4/bitlinks/{link}'
-    response = requests.get(url, headers={'Authorization': auth})
+    response = requests.get(url, headers={'Authorization': bitly_token})
     return response.ok
 
 
-def shorten_link(link, auth):
+def shorten_link(link, bitly_token):
     url = 'https://api-ssl.bitly.com/v4/shorten'
     payload = {'long_url': link}
-    response = requests.post(url, json=payload, headers={'Authorization': auth})
+    response = requests.post(url, json=payload, headers={'Authorization': bitly_token})
     response.raise_for_status()
     decoded_response = response.json()
     return decoded_response["link"]
 
 
-def count_click(bitlink, auth):
+def count_click(bitlink, bitly_token):
     url_count = f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary'
-    response_count = requests.get(url_count, headers={'Authorization': auth})
+    response_count = requests.get(url_count, headers={'Authorization': bitly_token})
     response_count.raise_for_status()
     decoded_response = response_count.json()
     return decoded_response['total_clicks']
@@ -31,7 +31,7 @@ def count_click(bitlink, auth):
 
 def main():
     load_dotenv()
-    BITOKEN = os.getenv('BITOKEN')
+    bit_token = os.getenv('BITOKEN')
     parser = argparse.ArgumentParser(description='Bitly command line application')
     parser.add_argument('-l', '--bitlink', help='enter your link here', type=str)
     args = parser.parse_args()
@@ -41,14 +41,14 @@ def main():
         link = input('Enter a link: ')
     parsed_link = urlparse(link)
     link2func = f'{parsed_link.hostname}{parsed_link.path}'
-    if is_bitlink(link2func, BITOKEN):
+    if is_bitlink(link2func, bit_token):
         try:
-            print(f'Total clicks: {count_click(link2func, BITOKEN)}')
+            print(f'Total clicks: {count_click(link2func, bit_token)}')
         except HTTPError as err:
             print(err)
     else:
         try:
-            print(shorten_link(link, BITOKEN))
+            print(shorten_link(link, bit_token))
         except HTTPError as err:
             print(err)
 
